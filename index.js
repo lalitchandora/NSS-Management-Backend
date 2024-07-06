c84cd1dc2a0b0978d4dc5034b5a315a04e0ec6b5
Lalit Chandora <lalit.n.chandora@gmail.com>
2024-07-06 12:26:31 +0530

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 require("dotenv").config({ path: "./config/.env" });
 const app = express();
 const PORT = 3000
-
+const cors = require("cors")
 const routes = require("./startup/routes");
+
 app.use(express.json());
+app.use(cors());
 app.use("/api", routes);
 require("./startup/database")()
 
